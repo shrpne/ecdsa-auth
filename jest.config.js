708a7a0f7d065d006d91db038eeaf99a1cdc5995
shrpne <shrpne@gmail.com>
2023-09-12 13:26:20 +0100

    init

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const esModules = [
+    'date-fns',
+    'lodash-es',
+    '@babel/runtime',
+    'axios-extensions',
+    'axios',
+    'camelcase-keys',
+    'camelcase',
+    'quick-lru',
+].join('|');
+
+export default {
+    moduleNameMapper: {
+        // '~(.*)$': '<rootDir>/$1',
+        // 'axios/lib/adapters/adapters.js': '<rootDir>/node_modules/axios/lib/adapters/adapters.js',
+    },
+    transform: {
+        '^.+\\.jsx?$': '<rootDir>/jest-babel.config.js',
+    },
+    transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+    // testEnvironment: 'node',
+    // collectCoverage: true,
+    // collectCoverageFrom: ["./src/**"],
+    // coverageReporters: ["lcov", "text"]
+};
---
+++ src/index.js
@@ -0,0 +1,88 @@
+import {toBuffer} from 'ethereumjs-util/dist/bytes.js';
+import {keccakFromString} from 'ethereumjs-util/dist/hash.js';
+import {publicToAddress} from 'ethereumjs-util/dist/account.js';
+import secp256k1 from 'secp256k1';
+
+const {ecdsaSign, ecdsaVerify, publicKeyCreate} = secp256k1;
+
+
+/**
+ * @param {object|string} data
+ * @param {string} privateKey
+ * @param {boolean} [includePublicKey=true]
+ * @return {string} - result consist of 64 byte signature + 1 byte recid + 33 byte compressed public key
+ */
+export function ecdsaAuthSign(data, privateKey, includePublicKey = true) {
+    const privateKeyBuffer = toBuffer(privateKey);
+    const dataHash = typeof data === 'string' ? keccakFromString(data) : hashObject(data);
+    // recid is 0-3 number @see https://ethereum.stackexchange.com/a/118342
+    const {signature, recid} = ecdsaSign(dataHash, privateKeyBuffer);
+
+    const publicKey = publicKeyCreate(privateKeyBuffer, true);
+    // result consist of 64 byte signature + 1 byte recid + 33 byte compressed public key
+    let result = concatTypedList([
+        signature,
+        [recid],
+        includePublicKey ? publicKey : [],
+    ]);
+
+    return bufferToString(Buffer.from(result));
+}
+
+/**
+ * @param {string} authResult - hex string
+ * @return {{signature: Buffer, publicKey: Buffer, recid: number}}
+ */
+export function ecdsaAuthParse(authResult) {
+    const signature = toBuffer(authResult).subarray(0, 64);
+    const recid = toBuffer(authResult)[64];
+    const publicKey = toBuffer(authResult).subarray(65);
+
+    return {signature, recid, publicKey};
+}
+
+export function ecdsaAuthVerify(data, authString) {
+    const {signature, recid, publicKey} = ecdsaAuthParse(authString);
+    const dataHash = hashObject(data);
+    const isValid = ecdsaVerify(signature, dataHash, publicKey);
+    if (isValid) {
+        return bufferToString(publicToAddress(publicKey, true));
+    } else {
+        return false;
+    }
+}
+
+
+/**
+ * @param {Array<Uint8Array|Array|Buffer>} list
+ * @return {Uint8Array}
+ */
+function concatTypedList(list) {
+    const totalLength = list.reduce((acc, value) => acc + value.length, 0);
+    let result = new Uint8Array(totalLength);
+    let offset = 0;
+    for (const array of list) {
+        result.set(array, offset);
+        offset += array.length;
+    }
+
+    return result;
+}
+
+
+
+/**
+ * @param {object} data
+ * @return {Buffer}
+ */
+export function hashObject(data) {
+    return keccakFromString(JSON.stringify(data));
+}
+
+/**
+ * @param {Buffer|Uint8Array} buf
+ * @return {string}
+ */
+export function bufferToString(buf) {
+    return '0x' + toBuffer(buf).toString('hex');
+}
